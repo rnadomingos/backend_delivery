201fd91526c3be1d13718c1d0ca3d5bdd5e8b761
Renan <rna.domingos@hotmail.com>
2022-03-27 13:11:58 -0300

    Inicial Backend Entregas

---
+++ readme.md
@@ -0,0 +1,31 @@
+# Backend Entregas
+
+    [X] Criar estrutura de projeto
+        [X] Prisma
+        [X] TS
+        [X] Bcrypt
+        [X] JWT
+        [X] Express
+    [X] Criar tabela deliveryman
+    [X] Criar tabela client
+    [X] Criar tabela deliveries
+    [X] Configurar express(server)
+    [] Criar cadastro de client
+    [] Criar autenticacao
+    [] Criar cadastro de deliveryman
+    [] Criar cadastro de entregas
+    [] Criar busca de entregas sem deliveryman
+    [] Criar update de entregas
+    [] Criar busca de entregas do client
+    [] Criar busca de entregas do deliverymay
+
+## Docker DB - Postgres
+   sudo docker run --name postgres \
+    -e POSTGRES_USER=delivery \
+    -e POSTGRES_PASSWORD=delivery \
+    -e POSTGRES_DB=delivery \
+    -p 5432:5432 \
+    -e PGDATA=/var/lib/postgresql/data/pgdata \
+    -v /home/renan/stgfile/Developer/projetos_nodejs/rocketseat/beckend_entregas/PGDATA:/var/lib/postgresql/data/pgdata \
+    -d \
+    postgres:alpine
---
+++ src/modules/clients/useCase/createClient/CreateClientUseCase.ts
@@ -0,0 +1,38 @@
+import { prisma } from '../../../../database/prismaClient'
+import { hash } from 'bcrypt'
+
+interface ICreateClient {
+    username: string,
+    password: string
+}
+
+
+export class CreateClientUseCase{
+
+    async execute({password, username}: ICreateClient) {
+        // Validar se o usuário existe
+        const clientExists = await prisma.clients.findFirst({
+           where: {
+               username: {
+                   equals: username,
+                   mode: 'insensitive'       
+               }
+            }
+        })
+
+        if (clientExists) {
+            throw new Error(`Client alredy exists: ${clientExists.username}`);
+        }
+        // Criptografar a senha
+        const hashPassword = await hash(password, 10);
+
+        // Salvar o client
+        const client = await prisma.clients.create({
+            data: {
+                username,
+                password: hashPassword
+            }
+        })
+        return client;
+    }
+}
